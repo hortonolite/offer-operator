98be3e5e47796b655023b2bb7b3041f55b47720c
Stanislav Spiridonov <stas@jresearch.org>
2025-10-15 10:55:45 +0200

    Fix controller setup and schema derives

--- src/crd.rs
+++ src/crd.rs
@@ -1,7 +1,8 @@
 use kube::CustomResource;
+use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-#[derive(CustomResource, Serialize, Deserialize, Clone, Debug)]
+#[derive(CustomResource, Serialize, Deserialize, Clone, Debug, JsonSchema)]
 #[kube(
     group = "offer.jresearch.ai",
     version = "v1",
